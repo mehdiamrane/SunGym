4f4ee2fd2b5892b8b7ee8d83b8107159ca4553d7
Mehdi Amrane <me.amrane@gmail.com>
2020-07-06 17:07:11 +0200

    Updated NavBar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,50 +1,98 @@
-import React, {useState} from 'react'
-import Link from 'next/link'
+import React, { useContext } from 'react';
+import Link from 'next/link';
+import { AuthContext } from 'context/auth';
+import Cookies from 'js-cookie';
+import Router from 'next/router';
+import cogoToast from 'cogo-toast';
+import toastOptions from 'helpers/toastOptions';
 
 function Navbar() {
+  const { userData, setUserData } = useContext(AuthContext);
 
-    const [isLogin, setIsLogin] = useState(true)
+  const logout = () => {
+    Cookies.remove('auth-token');
+    setUserData({ token: null, user: null });
+    cogoToast.success('Vous vous êtes déconnecté', toastOptions);
+    Router.push('/');
+  };
 
-   
-    if(isLogin === false){return (
-        <div>
-            <ul className='navbar'>
+  if (!userData.user) {
+    return (
+      <div>
+        <ul className='navbar'>
+          <div className='nav-left'>
+            <div className='acceuil-logo'></div>
+            <Link passHref href='/'>
+              <li>
+                <a>Acceuil</a>
+              </li>
+            </Link>
+            <Link passHref href='/tarifs'>
+              <li>
+                <a>Tarifs</a>
+              </li>
+            </Link>
+            <Link passHref href='/contact'>
+              <li>
+                <a>Nous contacter</a>
+              </li>
+            </Link>
+          </div>
 
-                <div className='nav-left'>
-                    <div className='acceuil-logo'></div>
-                    <Link passHref href='/'><li><a>Acceuil</a></li></Link>
-                    <Link passHref href='/tarifs' ><li><a>Tarifs</a></li></Link>
-                    <Link passHref href='/contact' ><li><a>Nous contacter</a></li></Link>
-                </div>
-                
-                <div className='nav-right'>
-                    <Link passHref href='/subscribe'><li><a>S'inscrire</a></li></Link>
-                    <Link passHref href='/login'><li onClick={()=>setIsLogin(true)}><a>Se connecter</a></li></Link>
-                </div>
+          <div className='nav-right'>
+            <Link passHref href='/register'>
+              <li>
+                <a>S'inscrire</a>
+              </li>
+            </Link>
+            <Link passHref href='/login'>
+              <li>
+                <a>Se connecter</a>
+              </li>
+            </Link>
+          </div>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <ul className='navbar'>
+          <div className='nav-left'>
+            <div className='acceuil-logo'></div>
+            <Link passHref href='/'>
+              <li>
+                <a>Acceuil</a>
+              </li>
+            </Link>
+            <Link passHref href='/tarifs'>
+              <li>
+                <a>Tarifs</a>
+              </li>
+            </Link>
+            <Link passHref href='/contact'>
+              <li>
+                <a>Nous contacter</a>
+              </li>
+            </Link>
+          </div>
 
-            </ul>
-        </div>
-    )} 
-    else { return (
-        <div>
-            <ul className='navbar'>
-
-                <div className='nav-left'>
-                    <div className='acceuil-logo'></div>
-                    <Link passHref href='/'><li><a>Acceuil</a></li></Link>
-                    <Link passHref href='/tarifs' ><li><a>Tarifs</a></li></Link>
-                    <Link passHref href='/contact' ><li><a>Nous contacter</a></li></Link>
-                </div>
-                
-                <div className='nav-right'>
-                    <Link passHref href='/account'><li><a>Mon Compte</a></li></Link>
-                    <Link passHref href='/' ><li onClick={()=>setIsLogin(false)}><a>Se Déconnecter</a></li></Link>
-                </div>
-
-            </ul>
-        </div>
-    )}
+          <div className='nav-right'>
+            <Link passHref href='/compte'>
+              <li>
+                <a>Mon Compte</a>
+              </li>
+            </Link>
+            <Link passHref href='/'>
+              <li onClick={logout}>
+                <a>Se Déconnecter</a>
+              </li>
+            </Link>
+          </div>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Navbar
-
+export default Navbar;
