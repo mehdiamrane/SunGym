9b69902c7784059c7771b15647e121b2789de6ae
Mehdi Amrane <me.amrane@gmail.com>
2020-07-06 19:28:51 +0200

    Updated the baseURL with the prod API url

--- helpers/baseURL.js
+++ helpers/baseURL.js
@@ -1,7 +1,7 @@
 let urls = {
   test: `http://localhost:5000`,
-  development: 'http://localhost:5000/',
-  production: 'https://your-production-url.com/',
+  development: 'https://sungym.herokuapp.com',
+  production: 'https://sungym.herokuapp.com',
 };
 
 const baseURL = urls[process.env.NODE_ENV];
