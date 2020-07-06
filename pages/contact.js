ec1279862f41cd4c3872a50321c6989b3da21157
Mehdi Amrane <me.amrane@gmail.com>
2020-07-06 18:50:49 +0200

    Updated contact form to work with API

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
+import api from 'helpers/api';
+import cogoToast from 'cogo-toast';
+import toastOptions from 'helpers/toastOptions';
 
 function Contact() {
+  const [email, setEmail] = useState();
+  const [nom, setNom] = useState();
+  const [tel, setTel] = useState();
+  const [message, setMessage] = useState();
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = { email, nom, message, tel };
+      await api.post('email', data);
+
+      cogoToast.success('Message envoyé avec succès', toastOptions);
+      setEmail();
+      setNom();
+      setTel();
+      setMessage();
+    } catch (err) {
+      if (err.response.data.msg) setError(err.response.data.msg);
+    }
+  };
   return (
     <div className='contact-page'>
       <Head>
@@ -11,15 +35,42 @@ function Contact() {
       </Head>
       <Navbar />
       <div className='contact-zone'>
-        <form className='contact-form'>
+        <form className='contact-form' onSubmit={submit}>
           <h2 className='h2-title'>NOUS CONTACTER</h2>
-          <input type='texte' placeholder='Votre Nom'></input>
+          <input
+            type='text'
+            id='nom'
+            name='nom'
+            placeholder='Votre nom'
+            required
+            onChange={(e) => setNom(e.target.value)}
+          />
+          <input
+            type='email'
+            id='email'
+            name='email'
+            placeholder='Votre email'
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type='number'
+            id='tel'
+            name='tel'
+            placeholder='Votre numéro de téléphone'
+            required
+            onChange={(e) => setTel(e.target.value)}
+          />
+          <textarea
+            type='text'
+            id='message'
+            name='message'
+            placeholder='Votre message'
+            required
+            onChange={(e) => setMessage(e.target.value)}
+          />
 
-          <input type='text' placeholder='Votre numéro de téléphone'></input>
-          <textarea placeholder='Votre message'></textarea>
-          <button className='btn-select' type='submit'>
-            ENVOYER
-          </button>
+          <input type='submit' value='ENVOYER' className='btn-select' />
         </form>
       </div>
       <Footer />
