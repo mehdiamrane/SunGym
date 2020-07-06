970eb613bcb0971684c8cb8629ff0a72dcd4d52d
Mehdi Amrane <me.amrane@gmail.com>
2020-07-06 13:11:08 +0200

    Modifications Login.js

--- pages/login.js
+++ pages/login.js
@@ -1,39 +1,36 @@
-import React from 'react'
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
+function Login() {
+  return (
+    <div>
+      <div className='subscribe-page'>
+        <Head>
+          <title>Se connecter ! - Sun Gym</title>
+        </Head>
+        <Navbar />
 
-function Login () {
-    return (
-        
-        <div>
-            <div className='subscribe-page'>
-                <Head>
-                    <title>Se connecter - Sun Gym</title>
-                </Head>
-                <Navbar/>
+        <form className='form-container'>
+          <h2 className='h2-title'>CONNEXION</h2>
 
-                <form className='form-container'>
-                    <h2 className='h2-title'>CONNEXION</h2>
-                    
-                    <div className='form-input-div'>
-                        <input type='email' name='email' placeholder='Email'></input>
-                    </div>
+          <div className='form-input-div'>
+            <input type='email' name='email' placeholder='Email'></input>
+          </div>
 
-                    <div className='form-input-div'>
-                        <input type='password' name='password' placeholder='Mot de passe'></input>
-                    </div>
-                    
-                    <button className='subscribe-btn' type='submit'>SE CONNECTER</button>
-                    
-                </form>
-                
+          <div className='form-input-div'>
+            <input type='password' name='password' placeholder='Mot de passe'></input>
+          </div>
 
-            </div>
-            <Footer/>
-        </div>
-    )
+          <button className='subscribe-btn' type='submit'>
+            SE CONNECTER
+          </button>
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default Login
+export default Login;
