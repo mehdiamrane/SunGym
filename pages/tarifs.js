2eec1340234bb49bc084fdf3bf339d8840d9c779
Mehdi Amrane <me.amrane@gmail.com>
2020-07-06 16:29:05 +0200

    Updated import paths

--- pages/tarifs.js
+++ pages/tarifs.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
-import Navbar from '../components/Navbar';
+import Navbar from 'components/Navbar';
 import Link from 'next/link';
-import Footer from '../components/Footer';
+import Footer from 'components/Footer';
 
 function Tarifs() {
   return (
