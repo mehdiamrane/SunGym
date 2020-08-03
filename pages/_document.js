7825ae758abd5af15abf4c5310b8bf4bed314faa
Mehdi Amrane <me.amrane@gmail.com>
2020-08-03 21:19:27 +0200

    Small fix + added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel='icon' type='image/png' href='/logo.png' />
           {/* polices de criture ici */}
           <link
             href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
